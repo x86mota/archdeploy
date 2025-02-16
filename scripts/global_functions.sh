2d6b75091a24bf2880c1c154fd1b35e326f14535
x86mota <felipecruz.d@proton.me>
2025-02-16 18:07:46 -0300

    Install Package

--- scripts/global_functions.sh
+++ scripts/global_functions.sh
@@ -22,11 +22,36 @@ function _IsPacmanAvailable {
 
 # Checks if package is available in AUR
 function _IsAURAvailable {
-    local AurHelper="$1"
-    local Pkg="$2"
+    local Pkg="$1"
     if ${AurHelper} -Si "${Pkg}" &>/dev/null; then
         return 0
     else
         return 1
     fi
+}
+
+# Install Package
+function _InstallPackage {
+    local Pkg="$1"
+    local Args="-S --noconfirm --needed"
+    local Msg="Installing ${Pkg}..."
+    local AURMsg="Installing ${Pkg} from AUR. This may take a while..."
+    
+    if _IsPacmanAvailable "${Pkg}"; then
+        echo -e "${Note} - ${Msg}"
+        sudo pacman "${Args}" "${Pkg}" &>/dev/null && {
+            echo -e "${Clear}${OK} - ${Msg}"
+            return 0
+        } || echo -e "${Clear}${Error} - ${Msg}" && exit 1
+    fi
+
+    if _IsAURAvailable "${Pkg}"; then
+        echo -e "${Note} - ${AURMsg}"
+        ${AurHelper} "${Args}" "${Pkg}" &>/dev/null && {
+            echo -e "${OK} - ${AURMsg}"
+            return 0
+        } || echo -e "${Clear}${Error} - ${AURMsg}" && exit 1
+    fi
+
+    echo -e "${Clear}${Error} - Unknown package ${Pkg}." && exit 1
 }
