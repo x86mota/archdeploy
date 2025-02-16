89c038ae9b68a29a9a1d8d2712c5f219051a57d0
x86mota <felipecruz.d@proton.me>
2025-02-16 18:26:42 -0300

    Get kernel headers

--- scripts/main.sh
+++ scripts/main.sh
@@ -4,3 +4,13 @@ source ./global_functions.sh
 source ./packages.sh
 source ./pacman.sh
 source ./aurhelper.sh
+
+# Get kernel headers
+if [[ -d "usr/lib/modules" ]]; then
+    for kernel in $(cat /usr/lib/modules/*/pkgbase); do
+        SYSTEM+=("${kernel}-headers")
+    done
+else
+    kernel=$(uname -s | tr '[:upper:]' '[:lower:]')
+    SYSTEM+=("${kernel}-headers")
+fi
