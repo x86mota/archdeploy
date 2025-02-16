a32ec57c158c6150bb4457184d16e6f2b2e29330
x86mota <felipecruz.d@proton.me>
2025-02-16 17:37:08 -0300

    initial commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Global Variables
+export DownloadDir="/tmp/hypr-arch"
+export Note='[\033[1;34mNOTE\033[0m]'
+export OK='[\033[1;32mOK\033[0m]'
+export Action='[\033[1;33mACTION\033[0m]'
+export Error='[\033[1;31mERROR\033[0m]'
+export Clear='\033[1A\033[K'
