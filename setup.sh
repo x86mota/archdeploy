36855fa5ed3eef668188fd222bf13a7557de5313
x86mota <felipecruz.d@proton.me>
2025-02-16 17:37:56 -0300

    add ascii art

--- setup.sh
+++ setup.sh
@@ -7,3 +7,22 @@ export OK='[\033[1;32mOK\033[0m]'
 export Action='[\033[1;33mACTION\033[0m]'
 export Error='[\033[1;31mERROR\033[0m]'
 export Clear='\033[1A\033[K'
+
+clear && echo -e "\033[1;32m
+                    -@                           
+                   .##@                          
+                  .####@                         
+                  @#####@                        
+                . *######@                       
+               .##@o@#####@                                                                ,,                           ,,  
+              /############@                      \`7MMF'  \`7MMF'                         \`7MM                         \`7MM  
+             /##############@             M         MM      MM                             MM                           MM  
+            @######@**%######@            M         MM      MM \`7M'   \`MF\`7MMpdMAo\`7Mb,od8 MM  ,6\"Yb. \`7MMpMMMb.   ,M\"\"bMM  
+           @######\`     %#####o       mmmmMmmmm     MMmmmmmmMM   VA   ,V   MM   \`Wb MM' \"' MM 8)   MM   MM    MM ,AP    MM  
+          @######@       ######%          M         MM      MM    VA ,V    MM    M8 MM     MM  ,pm9MM   MM    MM 8MI    MM  
+        -@#######h       ######@.\`        M         MM      MM     VVV     MM   ,AP MM     MM 8M   MM   MM    MM \`Mb    MM  
+       /#####h**\`\`       \`**%@####@               .JMML.  .JMML.   ,V      MMbmmd'.JMML. .JMML\`Moo9^Yo.JMML  JMML.\`Wbmd\"MML.
+      @H@*\`                    \`*%#@                              ,V       MM                                               
+     *\`                            \`*                          OOb\"      .JMML.                                             
+
+\033[0m"
