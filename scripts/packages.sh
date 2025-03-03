46a630a4c25b98c96b64d6b5cc728cdd776fd4d2
x86mota <felipecruz.d@proton.me>
2025-03-03 13:00:20 -0300

    chore: remove QT packages required by hyprland

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -104,3 +104,13 @@ Nvidia=(
     lib32-nvidia-utils
     lib32-vulkan-icd-loader
 )
+
+toRemove=(
+    hyprland-qtutils
+    hyprland-qt-support
+    qt6-base
+    qt6-declarative
+    qt6-wayland
+    qt6-svg
+    qt6-translations
+)
